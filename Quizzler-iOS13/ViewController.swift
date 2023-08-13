47186fa29a08e3ec77da3c0409f02b6121865a96
Mariana Piz <marianapiz0412@gmail.com>
2023-08-13 17:37:19 +0300

    + add connections

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
     }
 
 
