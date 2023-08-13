4e753a6b71924544eefb92cd9254916dafa4f886
Mariana Piz <marianapiz0412@gmail.com>
2023-08-13 19:34:36 +0300

    + change label text

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,11 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        "4 + 2 = 6",
+        "5 - 3 > 1",
+        "3 + 8 < 10"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
     }
 
 
